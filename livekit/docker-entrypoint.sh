f6583062179169b3aef8a6233a431ed1528826af
Patrick Noack <noackpat@gmail.com>
2024-11-13 11:07:28 +0100

    livekit fix turnserver

--- livekit/docker-entrypoint.sh
+++ livekit/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 sed -i "s|REPLACE_API_KEY|$LIVEKIT_API_KEY|g" /etc/livekit.yaml
 sed -i "s|REPLACE_API_SECRET|$LIVEKIT_API_SECRET|g" /etc/livekit.yaml
-sed -i "s|REPLACE_TURN_DOMAIN|$TURN_DOMAIN|g" /etc/livekit.yaml
+sed -i "s|REPLACE_TURN_DOMAIN|$LIVEKIT_TURN_DOMAIN|g" /etc/livekit.yaml
 sed -i "s|REPLACE_LIVEKIT_DOMAIN|$LIVEKIT_DOMAIN|g" /etc/livekit.yaml
 
 /livekit-server --config /etc/livekit.yaml 
