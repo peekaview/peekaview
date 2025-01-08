292482cab78e9d5c841a1143014d48154323c36d
Sebastian Igerl <igerlster@gmail.com>
2025-01-08 17:00:27 +0100

    Refactor notarization script to use require syntax and modularize functionality
    - Changed the import statement for the notarize function from ES6 import to CommonJS require syntax for compatibility.
    - Modularized the notarization process by encapsulating it within an exported async function, allowing for easier reuse and testing.
    - Updated the appPath to a specific path for the notarization process, enhancing clarity and functionality.

--- app/scripts/notarize.js
+++ app/scripts/notarize.js
@@ -1,8 +1,10 @@
-import { notarize } from '@electron/notarize';
+const { notarize } = require('@electron/notarize');
 
-await notarize({
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-    teamId: process.env.TEAMID, // Team ID for your developer team
-});
+module.exports = async function notarizeApp(appOutDir, appName) {
+    await notarize({
+        appPath: "dist/mac-arm64/peekaview.app", //dist/mac-arm64/peekaview.app was `${appOutDir}/${appName}.app`
+        appleId: process.env.APPLEID,
+        appleIdPassword: process.env.APPLEIDPASS,
+        teamId: process.env.TEAMID, // Team ID for your developer team
+    });
+};
