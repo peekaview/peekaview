756677a39b3222c922017573e65b4ed766e32cd7
Sebastian Igerl <igerlster@gmail.com>
2025-01-08 16:56:20 +0100

    Refactor GitHub Actions workflow and notarization script
    - Updated the release-app-native.yml workflow to include necessary environment variables for macOS builds, enhancing the notarization process.
    - Simplified the notarization script by removing unnecessary checks and directly utilizing the notarize function with the required parameters.
    - Ensured the inclusion of TEAMID in the environment variables for better team management during notarization.

--- app/scripts/notarize.js
+++ app/scripts/notarize.js
@@ -1,20 +1,8 @@
 import { notarize } from '@electron/notarize';
 
-exports.default = async function notarizing(context) {
-    const { electronPlatformName, appOutDir } = context;
-    if (electronPlatformName !== 'darwin') {
-        return;
-    }
-
-    const appName = context.packager.appInfo.productFilename;
-
-    return await notarize({
-        tool: 'notarytool',
-        teamId: process.env.APPLETEAMID,
-        appBundleId: 'com.limtec.peekaview',
-        appPath: `${appOutDir}/${appName}.app`,
-        appleId: process.env.APPLEID,
-        appleIdPassword: process.env.APPLEIDPASS,
-    });
-};
-// evtl auch angucken: https://github.com/electron/electron/issues/31496
+await notarize({
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+    teamId: process.env.TEAMID, // Team ID for your developer team
+});
