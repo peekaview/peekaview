5fed412e4c5a5e7d540060fe494482641f91eb33
Patrick Noack <pno@limtec.de>
2025-03-18 17:36:44 +0100

    Add service account key setup in docker-entrypoint.sh and update Dockerfile to include example key

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -5,5 +5,9 @@ export SERVER_NAME=${API_DOMAIN}:80
 export TURN_SHARED_SECRET=${TURN_SHARED_SECRET}
 export JWT_SECRET=${JWT_SECRET}
 
+if [ ! -f /storage/serviceAccountKey.json ]; then
+    cp -f /app/public/serviceAccountKey.json.example /storage/serviceAccountKey.json
+fi
+
 # Start FrankenPHP
 exec frankenphp run --config /etc/caddy/Caddyfile 
