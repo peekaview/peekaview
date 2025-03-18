e92c5e7dd1575cb4a7d9d63797ed3b9cafaa8538
Alexander Dauber <ada@limtec.de>
2025-03-18 17:48:18 +0100

    api correcting path

--- api/send.php
+++ api/send.php
@@ -7,7 +7,7 @@ use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
 
 // Initialize Firebase
-$factory = (new Factory)->withServiceAccount('../serviceAccountKey.json');
+$factory = (new Factory)->withServiceAccount('./storage/serviceAccountKey.json');
 $messaging = $factory->createMessaging();
 
 /**
